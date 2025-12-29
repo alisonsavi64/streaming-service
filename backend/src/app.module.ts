433c4ae5181368d0f0e5e694a6cdb3fad1dc980c
alison@movitech.com.br <alison@movitech.com.br>
2025-12-29 20:51:12 -0300

    simple login and register working

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'streaming',
+      password: 'streaming',
+      database: 'streaming',
+      autoLoadEntities: true,
+      synchronize: true
+    }),
     AuthModule,
   ],
 })
