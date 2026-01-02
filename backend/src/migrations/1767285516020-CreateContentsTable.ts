e41d238e17c1d1cdac7823286cd2a74437e9eb44
alison@movitech.com.br <alison@movitech.com.br>
2026-01-02 06:36:14 -0300

    adjust bug of not running migrations

--- backend/src/migrations/1767285516020-CreateContentsTable.ts
+++ backend/src/migrations/1767285516020-CreateContentsTable.ts
@@ -1,10 +1,10 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreateInitialTables1767285516019 implements MigrationInterface {
-    name = 'CreateInitialTables1767285516019'
+export class CreateContentsTable1767285516020 implements MigrationInterface {
+    name = 'CreateContentsTable1767285516020'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "email" character varying NOT NULL, "password_hash" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TYPE "contents_status_enum" AS ENUM ('UPLOADED', 'PROCESSING', 'PROCESSED', 'FAILED')`);
         await queryRunner.query(`CREATE TABLE "contents" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "title" character varying NOT NULL, "description" text NOT NULL, "status" "public"."contents_status_enum" NOT NULL DEFAULT 'UPLOADED', "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "processedAt" TIMESTAMP, "thumbnailUrl" character varying, "userId" uuid NOT NULL, CONSTRAINT "PK_b7c504072e537532d7080c54fac" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "contents" ADD CONSTRAINT "FK_191675b22eb3ee27cda4aeb0f5f" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
@@ -12,7 +12,7 @@ export class CreateInitialTables1767285516019 implements MigrationInterface {
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "contents" DROP CONSTRAINT "FK_191675b22eb3ee27cda4aeb0f5f"`);
         await queryRunner.query(`DROP TABLE "contents"`);
-        await queryRunner.query(`DROP TABLE "users"`);
+        await queryRunner.query(`DROP TYPE "contents_status_enum"`);
     }
 
 }
